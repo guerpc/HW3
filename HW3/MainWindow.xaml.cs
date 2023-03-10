c126767b9311360eaf37a20df742442b1ffa2085
Philip Guerrero <guerpc@farmingdale.edu>
2023-03-09 20:51:18 -0500

    Add project files.

---
+++ HW3/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using HW2DLL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DataContractJsonSerializer Serializer;
+        DataContractJsonSerializer inputSerializer;
+        //ObservableCollection<Publisher> p1;
+        Publisher p1;
+        Book b1;
+
+        public MainWindow()
+        {           
+            InitializeComponent();
+            p1 = new Publisher();
+            b1 = new Book();
+            DataContext = p1;
+
+        }
+
+
+
+        private void buttonOpenPubFile_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = ""; // Default file name
+            dlg.DefaultExt = ""; // Default file extension
+            dlg.Filter = "JSON files (.json)|*.json"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+              //  Publisher pT = new Publisher();
+                //ObservableCollection<Book> bCount = new ObservableCollection<Book>();
+                // Open document
+                string filename = dlg.FileName;
+                textBoxPublisherFileName.Text = filename;
+                FileStream pReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                inputSerializer = new DataContractJsonSerializer(typeof(Publisher));
+                p1 = (Publisher)inputSerializer.ReadObject(pReader);
+                pReader.Dispose();
+                //string pubName = p1.Name;
+                //textBoxPublisherNameDisplay.Text = pubName;
+                //p1.Name = textBoxPublisherNameDisplay.Text;
+                //textBoxPublisherNameDisplay.Text = p1.Name;
+                //string z = textBoxPublisherNameDisplay.Text;
+                //int bCount = p1.BookList.Count;
+                
+                DataContext = p1;
+                //pT.Books=p1.Books;
+                //textBoxBookCountDisplay.DataContext = p1.BookList;
+               // Binding b = new Binding();
+                //textBoxBookCountDisplay.SetBinding(TextBox.TextProperty, b);
+            }
+
+        }
+
+        private void FindBook_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = textBoxPublisherFileName.Text;
+            FileStream pReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            inputSerializer = new DataContractJsonSerializer(typeof(Publisher));
+            p1 = (Publisher)inputSerializer.ReadObject(pReader);
+            pReader.Dispose();
+
+            b1 = p1.findBook(textBoxTargetBookTitle.Text);
+            /*
+            foreach (Book bk in p1.Books)
+            {
+                
+            }
+            */
+            textBoxBookTitle.DataContext = b1;
+            textBoxBookPrice.DataContext = b1;
+            listViewAuthors.DataContext = b1;
+            
+
+            //  p1.findBook();
+        }
+        /*
+        private void listViewBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            string filename = textBoxPublisherFileName.Text;
+            textBoxPublisherFileName.Text = filename;
+            FileStream pReader = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            inputSerializer = new DataContractJsonSerializer(typeof(Publisher));
+            p1 = (Publisher)inputSerializer.ReadObject(pReader);
+            pReader.Dispose();
+        }*/
+    }
+}
---
+++ HW3/OpenFileButton.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW3
+{
+	
+	public partial class OpenFileButton : Window
+	{
+		/*string FileName = "";
+
+		public OpenFileButton()
+		{
+			//InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			FileStream f;
+
+			MainWindow openFileDialog = new MainWindow();
+			if (openFileDialog.ShowDialog() == true)
+				textBoxPublisherFileName.Text = File.ReadAllText(openFileDialog.FileName);
+
+
+		}*/
+	}
+}
+
+
+
+		/*string FileName;
+		// Configure open file dialog box
+		Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+		
+		dlg. = "Document"; // Default file name
+		dlg.DefaultExt = ".txt"; // Default file extension
+		dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+		// Show open file dialog box
+		Nullable<bool> result = dlg.ShowDialog();
+
+		// Process open file dialog box results
+		
+		if (result == true)
+		
+			// Open document
+			string filename = dlg.FileName;*/
+
+		//	private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+		//	{
+		//		OpenFileDialog openFileDialog = new OpenFileDialog();
+		//		if (openFileDialog.ShowDialog() == true)
+		//			txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+		//}
+
+		//https://www.wpf-tutorial.com/dialogs/the-openfiledialog/
+	
+
+	
+
